e1abfba9d88130a9a65a8ffe481b40d0e978d66c
nigranac <can.argin.16@gmail.com>
2020-11-23 03:10:20 +0300

    Indicator

--- src/Carousel.js
+++ src/Carousel.js
@@ -48,6 +48,39 @@ const DATA = [
 ];
 
 
+const Indicator = ({scrollX}) => {
+    return (
+      <View style={{position: 'absolute', bottom: 100, flexDirection: 'row'}}>
+        {DATA.map((_, i) => {
+          const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
+          const scale = scrollX.interpolate({
+            inputRange,
+            outputRange: [0.8, 1.4, 0.8],
+            extrapolate: 'clamp',
+          });
+          const opacity = scrollX.interpolate({
+            inputRange,
+            outputRange: [0.6, 0.9, 0.6],
+            extrapolate: 'clamp',
+          });
+          return (
+            <Animated.View
+              key={`indicator-${i}`}
+              style={{
+                height: 15,
+                width: 15,
+                borderRadius: 7.5,
+                backgroundColor: '#fff',
+                margin: 10,
+                opacity,
+                transform: [{scale}],
+              }}></Animated.View>
+          );
+        })}
+      </View>
+    );
+  };
+
 const Carousel = () => {
     const scrollX = useRef(new Animated.Value(0)).current;
   
